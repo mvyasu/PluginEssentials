83a9b9186279a1bc604b8773f4fbbc6dd666b48a
boatbomber <arbitrarysoandso@gmail.com>
2022-04-07 17:31:34 -0400

    Add iconbutton, progressbar, shadow

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # PluginEssentials
 [![GitHub Issues](https://img.shields.io/github/issues/mvyasu/PluginEssentials.svg)](https://github.com/mvyasu/PluginEssentials/issues)
-[![GitHub Stars](https://img.shields.io/github/stars/mvyasu/PluginEssentials.svg)](https://github.com/mvyasu/PluginEssentials/stargazers) 
+[![GitHub Stars](https://img.shields.io/github/stars/mvyasu/PluginEssentials.svg)](https://github.com/mvyasu/PluginEssentials/stargazers)
 [![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
 
 This is a repository with components that I find essential to making plugins with [Fusion](https://github.com/Elttob/Fusion). You'll find in the src folder that there's two folders. Each one has a specific set of components which you can learn more about below.
@@ -27,12 +27,14 @@ This is a Fusion port of [StudioComponents](https://github.com/sircfenner/Studio
 | [BaseButton](https://github.com/mvyasu/PluginEssentials/blob/main/src/StudioComponents/BaseButton.lua) | A TextButton that reacts to different states like hover, press, and disabled. | TextButton |
 | [Button](https://github.com/mvyasu/PluginEssentials/blob/main/src/StudioComponents/Button.lua) | BaseButton, but with a different name. | TextButton |
 | [MainButton](https://github.com/mvyasu/PluginEssentials/blob/main/src/StudioComponents/MainButton.lua) | BaseButton, but uses the style of a main button, which is usually a blue background color. | TextButton |
+| [IconButton](https://github.com/mvyasu/PluginEssentials/blob/main/src/StudioComponents/IconButton.lua) | A button with an image on it instead of text. | TextButton |
 | [Checkbox](https://github.com/mvyasu/PluginEssentials/blob/main/src/StudioComponents/Checkbox.lua) | A checkbox that reacts to different states like hover, checked, indeterminate, and disabled. | Frame |
+| [Shadow](https://github.com/mvyasu/PluginEssentials/blob/main/src/StudioComponents/Shadow.lua) | A subtle shadow positioned on the side of your choosing | ImageLabel |
 | [Dropdown](https://github.com/mvyasu/PluginEssentials/blob/main/src/StudioComponents/Dropdown/init.lua) | A button that shows the selected item and a list of items in a dropdown when clicking on it. | Frame |
 | [ColorPicker](https://github.com/mvyasu/PluginEssentials/blob/main/src/StudioComponents/ColorPicker.lua) | An HSV color picker that uses a value slider. | Frame |
 | [ScrollFrame](https://github.com/mvyasu/PluginEssentials/blob/main/src/StudioComponents/ScrollFrame/init.lua) | A custom scrolling frame that doesn't use Roblox's ScrollingFrame. | Frame |
 | [Slider](https://github.com/mvyasu/PluginEssentials/blob/main/src/StudioComponents/Slider.lua) | A slider that reacts to being dragged. | TextButton |
-| [TextInput](https://github.com/mvyasu/PluginEssentials/blob/main/src/StudioComponents/TextInput.lua) | A TextBox that reacts to states like hover, focus, and disabled. | TextBox |
+| [ProgressBar](https://github.com/mvyasu/PluginEssentials/blob/main/src/StudioComponents/ProgressBar.lua) | A bar that fills up with an accent color. | TextBox |
 | [LimitedTextInput](https://github.com/mvyasu/PluginEssentials/blob/main/src/StudioComponents/LimitedTextInput.lua) | TextInput, but contains properties for limiting the length of input to the TextBox. | TextBox |
 | [VerticalExpandingList](https://github.com/mvyasu/PluginEssentials/blob/main/src/StudioComponents/VerticalExpandingList.lua) | A Frame that uses AutomaticSize for the Y axis and contains a UIListLayout. | Frame |
 | [VerticalCollapsibleSection](https://github.com/mvyasu/PluginEssentials/blob/main/src/StudioComponents/VerticalCollapsibleSection.lua) | A frame that contains a header and a content frame. The content frame can be collapsed by clicking on the header.  | Frame |
---
+++ src/StudioComponents/IconButton.lua
@@ -0,0 +1,123 @@
+local Plugin = script:FindFirstAncestorWhichIsA("Plugin")
+local Fusion = require(Plugin:FindFirstChild("Fusion", true))
+
+local StudioComponents = script.Parent
+local StudioComponentsUtil = StudioComponents:FindFirstChild("Util")
+
+local BoxBorder = require(StudioComponents.BoxBorder)
+
+local getState = require(StudioComponentsUtil.getState)
+local themeProvider = require(StudioComponentsUtil.themeProvider)
+local constants = require(StudioComponentsUtil.constants)
+local unwrap = require(StudioComponentsUtil.unwrap)
+local types = require(StudioComponentsUtil.types)
+local stripProps = require(StudioComponentsUtil.stripProps)
+
+local New = Fusion.New
+local Value = Fusion.Value
+local Children = Fusion.Children
+local Computed = Fusion.Computed
+local OnEvent = Fusion.OnEvent
+local Hydrate = Fusion.Hydrate
+local Spring = Fusion.Spring
+
+local COMPONENT_ONLY_PROPERTIES = {
+	"ImageColorStyle",
+	"BackgroundColorStyle",
+	"BorderColorStyle",
+	"Activated",
+	"Enabled",
+	"Icon",
+}
+
+type styleGuideColorInput = (Enum.StudioStyleGuideColor | types.StateObject<Enum.StudioStyleGuideColor>)?
+
+export type IconButtonProperties = {
+	Activated: (() -> nil)?,
+	Enabled: (boolean | types.StateObject<boolean>)?,
+	TextColorStyle: styleGuideColorInput,
+	BackgroundColorStyle: styleGuideColorInput,
+	BorderColorStyle: styleGuideColorInput,
+	[any]: any,
+}
+
+return function(props: IconButtonProperties): TextButton
+	local isEnabled = getState(props.Enabled, true)
+	local isHovering = Value(false)
+	local isPressed = Value(false)
+
+	local modifier = Computed(function()
+		local isSelected = unwrap(props.Selected)
+		local isDisabled = not unwrap(isEnabled)
+		local isHovering = unwrap(isHovering)
+		local isPressed = unwrap(isPressed)
+		if isDisabled then
+			return Enum.StudioStyleGuideModifier.Disabled
+		elseif isSelected then
+			return Enum.StudioStyleGuideModifier.Selected
+		elseif isPressed then
+			return Enum.StudioStyleGuideModifier.Pressed
+		elseif isHovering then
+			return Enum.StudioStyleGuideModifier.Hover
+		end
+		return Enum.StudioStyleGuideModifier.Default
+	end)
+
+	local newBaseButton = BoxBorder {
+		Color = Spring(themeProvider:GetColor(props.BorderColorStyle or Enum.StudioStyleGuideColor.CheckedFieldBorder, modifier), 40),
+
+		[Children] = New "TextButton" {
+			Name = "IconButton",
+			Size = UDim2.fromScale(1, 1),
+			Text = "",
+			BackgroundColor3 = Spring(themeProvider:GetColor(props.BackgroundColorStyle or Enum.StudioStyleGuideColor.Button, modifier), 40),
+			AutoButtonColor = false,
+
+			[OnEvent "InputBegan"] = function(inputObject)
+				if not unwrap(isEnabled) then
+					return
+				elseif inputObject.UserInputType == Enum.UserInputType.MouseMovement then
+					isHovering:set(true)
+				elseif inputObject.UserInputType == Enum.UserInputType.MouseButton1 then
+					isPressed:set(true)
+				end
+			end,
+			[OnEvent "InputEnded"] = function(inputObject)
+				if not unwrap(isEnabled) then
+					return
+				elseif inputObject.UserInputType == Enum.UserInputType.MouseMovement then
+					isHovering:set(false)
+				elseif inputObject.UserInputType == Enum.UserInputType.MouseButton1 then
+					isPressed:set(false)
+				end
+			end,
+			[OnEvent "Activated"] = (function()
+				if props.Activated then
+					return function()
+						if unwrap(isEnabled, false) then
+							isHovering:set(false)
+							isPressed:set(false)
+							props.Activated()
+						end
+					end
+				end
+			end)(),
+
+			[Children] = {
+				New "ImageLabel" {
+					Name = "Icon",
+					BackgroundTransparency = 1,
+					Size = UDim2.fromScale(0.8, 0.8),
+					Position = UDim2.fromScale(0.5, 0.5),
+					AnchorPoint = Vector2.new(0.5, 0.5),
+					ScaleType = Enum.ScaleType.Fit,
+					ImageColor3 = Spring(themeProvider:GetColor(props.ImageColorStyle or Enum.StudioStyleGuideColor.ButtonText, modifier), 40),
+					Image = props.Icon,
+				},
+			},
+		}
+	}
+
+	local hydrateProps = stripProps(props, COMPONENT_ONLY_PROPERTIES)
+	return Hydrate(newBaseButton)(hydrateProps)
+end
