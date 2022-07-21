22f92645535a087fab122ca927cda1af1e3fba8c
Yasu Yoshida <fapmartinez@gmail.com>
2022-07-21 15:30:35 -0700

    ToolbarButton Active
    When creating a ToolbarButton, you can provide a state object for Active and behind the scenes :SetActive() will be called through an Observer.

--- src/PluginComponents/ToolbarButton.lua
+++ src/PluginComponents/ToolbarButton.lua
@@ -1,6 +1,14 @@
 local Plugin = script:FindFirstAncestorWhichIsA("Plugin")
 local Fusion = require(Plugin:FindFirstChild("Fusion", true))
 
+local PluginComponents = script.Parent
+local StudioComponents = PluginComponents.Parent:FindFirstChild("StudioComponents")
+local StudioComponentsUtil = StudioComponents:FindFirstChild("Util")
+
+local unwrap = require(StudioComponentsUtil.unwrap)
+local types = require(StudioComponentsUtil.types)
+
+local Observer = Fusion.Observer
 local Hydrate = Fusion.Hydrate
 
 local COMPONENT_ONLY_PROPERTIES = {
@@ -8,11 +16,12 @@ local COMPONENT_ONLY_PROPERTIES = {
 	"Name",
 	"Image",
 	"Toolbar",
+	"Active",
 }
 
 type ToolbarProperties = {
+	Active: types.CanBeState<boolean>?,
 	Toolbar: PluginToolbar,
-	ClickableWhenViewportHidden: boolean?,
 	ToolTip: string,
 	Image: string,
 	Name: string,
@@ -25,6 +34,15 @@ return function(props: ToolbarProperties)
 		props.ToolTip,
 		props.Image
 	)
+
+	if props.Active~=nil then
+		toolbarButton:SetActive(unwrap(props.Active))
+		if unwrap(props.Active)~=props.Active then
+			Plugin.Unloading:Connect(Observer(props.Active):onChange(function()
+				toolbarButton:SetActive(unwrap(props.Active, false))
+			end))
+		end
+	end
 
 	local hydrateProps = table.clone(props)
 	for _,propertyName in pairs(COMPONENT_ONLY_PROPERTIES) do
